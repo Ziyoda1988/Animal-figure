12862ddf64f240b8adb07d4fd0897209e4d6bf14
Ziyoda Muslimova <121525223+Ziyoda1988@users.noreply.github.com>
2023-02-22 16:38:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+   System.out.println("            '__'");
+        System.out.println("            (\u00a9\u00a9)");
+        System.out.println( "    /========\\/");
+        System.out.println("   / || \u00a4\u00a4 ||  ");
+        System.out.println("  *  ||----||");
+        System.out.println("     \u00a5\u00a5    \u00a5\u00a5");
+        System.out.println("     \"\"    \"\" ");
+    // Your animal figure here
+
+  }
+}
